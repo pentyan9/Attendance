8a4f89c085aba4c2e16921fdb77ee73977aad319
pentyan9 <oaulth@gmail.com>
2023-12-07 04:18:33 +0900

    first commit

---
+++ src/test/java/hayaryooo/site/Attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package hayaryooo.site.Attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
