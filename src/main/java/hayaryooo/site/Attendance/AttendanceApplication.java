7d53b850c113dea44ed695dcfc72eddc211254de
pentyan9 <oaulth@gmail.com>
2023-12-10 10:26:12 +0900

    configを追加

--- src/main/java/hayaryooo/site/Attendance/AttendanceApplication.java
+++ src/main/java/hayaryooo/site/Attendance/AttendanceApplication.java
@@ -1,9 +1,8 @@
 package hayaryooo.site.Attendance;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AttendanceApplication {
 
 	public static void main(String[] args) {
